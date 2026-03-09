d6ea86d8fe1ad02df4865dace595b4d78cfad72d
Alexandru Ghiura <ghalex@gmail.com>
2026-03-09 10:25:34 +0000

    feat: add sqlalchemy to project

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World22222"}
+
 def main():
-    print("Hello from chatbox!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
